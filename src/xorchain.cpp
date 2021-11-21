9ce28208b3718996d82ea15b49825f9ae3676873
Andy Rose <arose@cern.ch>
2021-11-21 15:16:22 +0000

    Add xor chain

---
+++ src/xorchain.cpp
@@ -0,0 +1,34 @@
+#include <stdint.h>
+#include <iostream>
+
+#include <array>
+
+
+using FnPtr64 = uint64_t (*)();
+
+template< int N >
+class xorchain
+{
+private:
+  std::array< uint64_t , N > A , B ;
+  FnPtr64 mGenA , mGenB;
+
+public:
+  xorchain( FnPtr64 aGenA , FnPtr64 aGenB ) : mGenA( aGenA ) , mGenB( aGenB )
+  {}
+
+  inline void operator() ()
+  {
+    B[0] = mGenA() xor A[1];
+    for( int i(1) ; i!= N-2 ; ++i ) B[i] = A[i-1] xor A[i+1];
+    B[N-1] = A[N-2] xor mGenB();
+
+    A.swap( B );
+  }
+
+  inline uint64_t operator[] ( const uint32_t& aIndex )
+  {
+    return A[aIndex];
+  }
+
+};
